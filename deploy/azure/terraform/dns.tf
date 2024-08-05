d79491230b2883f777b8af01da19e345fda2c703
Jack Blower <Jack@elvenspellmaker.co.uk>
2024-08-05 11:46:54 +0100

    feat: starts the pipeline for nx AWS

--- deploy/azure/terraform/dns.tf
+++ deploy/azure/terraform/dns.tf
@@ -1,17 +1,4 @@
 ###
-# Data
-###
-data "azurerm_dns_zone" "default" {
-  name                = var.dns_zone
-  resource_group_name = var.dns_zone_rg
-}
-
-data "azurerm_public_ip" "application_gateway" {
-  name                = var.app_gateway_frontend_ip_name
-  resource_group_name = var.app_gateway_frontend_ip_rg
-}
-
-###
 # Resources
 ###
 resource "azurerm_dns_a_record" "stacks_app" {
@@ -19,5 +6,5 @@ resource "azurerm_dns_a_record" "stacks_app" {
   zone_name           = data.azurerm_dns_zone.default.name
   resource_group_name = data.azurerm_dns_zone.default.resource_group_name
   ttl                 = var.dns_a_record_ttl
-  records             = [data.azurerm_public_ip.application_gateway.ip_address]
+  target_resource_id  = data.azurerm_public_ip.application_gateway.id
 }
